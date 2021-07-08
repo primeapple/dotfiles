b1b5f571fe15412c76a0c52b129720477f80f1cc
primeapple <toni.mueller@datameer.com>
2021-07-08 11:48:25 +0200

    added fish stuff

---
+++ .config/fish/functions/gw.fish
@@ -0,0 +1,3 @@
+function gw --wraps=./gradlew --description 'alias gw=./gradlew'
+  ./gradlew $argv; 
+end
---
+++ .config/fish/functions/vim.fish
@@ -0,0 +1,3 @@
+function vim --wraps=neovim --wraps=nvim --description 'alias vim=nvim'
+  nvim $argv; 
+end
