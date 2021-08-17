874364c6caeb680ca37c458869a11dc9a34c7780
primeapple <toni.mueller@datameer.com>
2021-08-17 09:18:15 +0200

    added some dotfiles, added instructions for rvm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
+source  ~/.profile
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 rvm default
-set -gx EDITOR nvim
-set -gx VISUAL nvim
+starship init fish | source
