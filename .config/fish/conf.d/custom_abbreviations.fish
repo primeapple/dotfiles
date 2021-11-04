ed03905d750f2ff8193fc9972360f0da9f52b5f5
primeapple <toni.mueller.web@mailbox.org>
2021-11-04 19:24:11 +0100

    added checks if abbreviation can be applied

--- .config/fish/conf.d/custom_abbreviations.fish
+++ .config/fish/conf.d/custom_abbreviations.fish
@@ -5,7 +5,14 @@ if status --is-interactive
 	abbr --add --global dm:validate './gradlew spotlessApply checkstyleMain checkstyleTest spotbugsTest checkApiChanges'
 	abbr --add --global gpu 'git push -u origin HEAD'
 	abbr --add --global gw './gradlew -Dconfig.profile=dev-local'
-	abbr --add --global ls exa
 	abbr --add --global mv 'mv -i'
-	abbr --add --global vim nvim
+	if command -q exa
+		abbr --add --global ls exa
+	end
+	if command -q nvim
+		abbr --add --global vim nvim
+	end
+	if command -q bat
+		abbr --add --global cat bat
+	end
 end
