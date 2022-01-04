48073db13aabf0f157e88bc64a07ab7dca07d9f1
primeapple <toni.mueller.web@mailbox.org>
2022-01-04 10:07:20 +0100

    added encryption for work abbreviations

--- .config/fish/conf.d/custom_abbreviations.fish
+++ .config/fish/conf.d/custom_abbreviations.fish
@@ -1,9 +1,6 @@
 if status --is-interactive
 	abbr --add --global .. 'cd ..'
 	abbr --add --global dc docker-compose
-	abbr --add --global dm:compile './gradlew compileJava --rerun-tasks --no-build-cache'
-	abbr --add --global dm:validate './gradlew spotlessApply checkstyleMain checkstyleTest spotbugsTest checkApiChanges'
-	abbr --add --global gw './gradlew -Dconfig.profile=dev-local'
 	abbr --add --global mv 'mv -i'
 	abbr --add --global g 'git'
 	abbr --add --global reload 'source ~/.config/fish/config.fish'
