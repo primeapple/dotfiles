f7ce09fbf337f373702b3386080d2c4df7742277
primeapple <toni.mueller.web@mailbox.org>
2023-02-08 16:08:11 +0100

    added fish hotkey for repainting/tidy screen

--- .config/fish/functions/fish_hybrid_key_bindings.fish
+++ .config/fish/functions/fish_hybrid_key_bindings.fish
@@ -2,10 +2,14 @@
 function fish_hybrid_key_bindings --description \
 "Vi-style bindings that inherit emacs-style bindings in all modes"
     for mode in default insert visual
-        fish_default_key_bindings -M $mode
+        fish_default_key_bindings --mode $mode
     end
     fish_vi_key_bindings --no-erase
 
+    for mode in default insert visual
+        # 't' stands for tidy
+        bind --mode $mode \ct 'echo -n (clear | string replace \e\[3J ""); commandline -f repaint'
+    end
     bind yy fish_clipboard_copy
     bind --mode visual y fish_clipboard_copy
     bind p fish_clipboard_paste
@@ -13,4 +17,5 @@ function fish_hybrid_key_bindings --description \
     bind L end-of-line
     bind --mode visual H beginning-of-line
     bind --mode visual L end-of-line
+
 end
