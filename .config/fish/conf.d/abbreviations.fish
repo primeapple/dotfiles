000dbc0dae4e5758f0902238abe3325217bcdfa4
primeapple <toni.mueller.web@mailbox.org>
2023-02-15 22:37:27 +0100

    added feature flag abbreviations for work, removed unneeded --global flag

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -1,79 +1,79 @@
 if status --is-interactive
     # applications
-	abbr --add --global dc docker-compose
-	abbr --add --global mv 'mv -i'
-	abbr --add --global reload 'source ~/.config/fish/config.fish'
-	abbr --add --global ls exa
-	abbr --add --global vim nvim
-	abbr --add --global . 'nvim .'
-	abbr --add --global cat bat
-	abbr --add --global find fd
-	abbr --add --global cht cht.sh
-    abbr --add --global record 'wf-recorder -g "$(slurp)"'
-    abbr --add --global screen 'grim -g "$(slurp)" screenshot.png'
+	abbr --add dc docker-compose
+	abbr --add mv 'mv -i'
+	abbr --add reload 'source ~/.config/fish/config.fish'
+	abbr --add ls exa
+	abbr --add vim nvim
+	abbr --add . 'nvim .'
+	abbr --add cat bat
+	abbr --add find fd
+	abbr --add cht cht.sh
+    abbr --add record 'wf-recorder -g "$(slurp)"'
+    abbr --add screen 'grim -g "$(slurp)" screenshot.png'
 
     # npm related ones
-    abbr --add --global n 'npm'
-    abbr --add --global nt 'npm test --'
-    abbr --add --global nrt 'npm run test'
-    abbr --add --global nrw 'npm run watch'
-    abbr --add --global nrl 'npm run lint'
-    abbr --add --global nrd 'npm run docs'
-    abbr --add --global nrp 'npm run prettify -- .'
+    abbr --add n 'npm'
+    abbr --add nt 'npm test --'
+    abbr --add nrt 'npm run test'
+    abbr --add nrw 'npm run watch'
+    abbr --add nrl 'npm run lint'
+    abbr --add nrd 'npm run docs'
+    abbr --add nrp 'npm run prettify -- .'
 
     # git/yadm related ones
     for tuples in g,git y,yadm
         echo $tuples | read -d , abb cmd
-        abbr --add --global $abb $cmd
-        abbr --add --global $abb'a' $cmd 'add'
-        abbr --add --global $abb'aa' $cmd 'add --all'
-        abbr --add --global $abb'au' $cmd 'add -u'
-        abbr --add --global $abb'c' $cmd 'commit -v'
-        abbr --add --global $abb'can' $cmd 'commit --amend --no-edit'
-        abbr --add --global $abb'r' $cmd 'reset'
-        abbr --add --global $abb'rs' $cmd 'restore'
-        abbr --add --global $abb'rst' $cmd 'restore --staged'
-        abbr --add --global $abb'rb' $cmd 'rebase'
-        abbr --add --global $abb'rba' $cmd 'rebase --abort'
-        abbr --add --global $abb'rbc' $cmd 'rebase --continue'
-        abbr --add --global $abb'rc' $cmd 'recent'
-        abbr --add --global $abb'd' $cmd 'diff'
-        abbr --add --global $abb'ds' $cmd 'diff --staged'
-        abbr --add --global $abb'f' $cmd 'fetch'
-        abbr --add --global $abb'pl' $cmd 'pull'
-        abbr --add --global $abb'p' $cmd 'push'
-        abbr --add --global $abb'pf' $cmd 'push --force'
-        abbr --add --global $abb'l' $cmd 'log --pretty=format:"%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]" --decorate --date=relative'
-        abbr --add --global $abb'lg' $cmd 'log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
-        abbr --add --global $abb'ls' $cmd 'log --pretty=format:"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn]" --decorate --numstat'
-        abbr --add --global $abb's' $cmd 'status --short --branch'
-        abbr --add --global $abb'st' $cmd 'stash'
-        abbr --add --global $abb'stp' $cmd 'stash pop'
-        abbr --add --global $abb'std' $cmd 'stash drop'
-        abbr --add --global $abb'stl' $cmd 'stash list'
-        abbr --add --global $abb'sts' $cmd 'stash show'
-        abbr --add --global $abb'sta' $cmd 'stash apply'
+        abbr --add $abb $cmd
+        abbr --add $abb'a' $cmd 'add'
+        abbr --add $abb'aa' $cmd 'add --all'
+        abbr --add $abb'au' $cmd 'add -u'
+        abbr --add $abb'c' $cmd 'commit -v'
+        abbr --add $abb'can' $cmd 'commit --amend --no-edit'
+        abbr --add $abb'r' $cmd 'reset'
+        abbr --add $abb'rs' $cmd 'restore'
+        abbr --add $abb'rst' $cmd 'restore --staged'
+        abbr --add $abb'rb' $cmd 'rebase'
+        abbr --add $abb'rba' $cmd 'rebase --abort'
+        abbr --add $abb'rbc' $cmd 'rebase --continue'
+        abbr --add $abb'rc' $cmd 'recent'
+        abbr --add $abb'd' $cmd 'diff'
+        abbr --add $abb'ds' $cmd 'diff --staged'
+        abbr --add $abb'f' $cmd 'fetch'
+        abbr --add $abb'pl' $cmd 'pull'
+        abbr --add $abb'p' $cmd 'push'
+        abbr --add $abb'pf' $cmd 'push --force'
+        abbr --add $abb'l' $cmd 'log --pretty=format:"%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]" --decorate --date=relative'
+        abbr --add $abb'lg' $cmd 'log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
+        abbr --add $abb'ls' $cmd 'log --pretty=format:"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn]" --decorate --numstat'
+        abbr --add $abb's' $cmd 'status --short --branch'
+        abbr --add $abb'st' $cmd 'stash'
+        abbr --add $abb'stp' $cmd 'stash pop'
+        abbr --add $abb'std' $cmd 'stash drop'
+        abbr --add $abb'stl' $cmd 'stash list'
+        abbr --add $abb'sts' $cmd 'stash show'
+        abbr --add $abb'sta' $cmd 'stash apply'
     end
     # only git
-    abbr --add --global gpb 'git publish'
-    abbr --add --global gb 'git branch'
-    abbr --add --global gbd 'git branch -d'
-    abbr --add --global gbm 'git branch -m'
-    abbr --add --global gco 'git checkout'
-    abbr --add --global gc- 'git checkout -'
-    abbr --add --global gcb 'git checkout -b'
-    abbr --add --global gcd 'git checkout dev'
-    abbr --add --global gcm 'git checkout main'
-    abbr --add --global grbd 'git rebase dev'
-    abbr --add --global grbm 'git rebase main'
+    abbr --add gpb 'git publish'
+    abbr --add gb 'git branch'
+    abbr --add gbd 'git branch -d'
+    abbr --add gbm 'git branch -m'
+    abbr --add gco 'git checkout'
+    abbr --add gc- 'git checkout -'
+    abbr --add gcb 'git checkout -b'
+    abbr --add gcd 'git checkout dev'
+    abbr --add gcm 'git checkout main'
+    abbr --add grbd 'git rebase dev'
+    abbr --add grbm 'git rebase main'
 
     # taskwarrior related ones
     # https://www.reddit.com/r/taskwarrior/comments/uvwqlz/share_your_aliases/
-    abbr --add --global t 'task'
-    abbr --add --global tui 'taskwarrior-tui'
-    abbr --add --global ts 'task sync'
-    abbr --add --global ta 'task add'
-    abbr --add --global tn 'task +next'
-    abbr --add --global tin 'clear; task inbox'
-    abbr --add --global tal 'task add dep:"$(task +LATEST uuids)"'
+    abbr --add t 'task'
+    abbr --add tui 'taskwarrior-tui'
+    abbr --add ts 'task sync'
+    abbr --add ta 'task add'
+    abbr --add tn 'task +next'
+    abbr --add tin 'clear; task inbox'
+    abbr --add tal 'task add dep:"$(task +LATEST uuids)"'
 end
