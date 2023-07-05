4b5a87ecf652f0245c4200fc19cab2c668825542
primeapple <toni.mueller.web@mailbox.org>
2023-07-05 08:41:03 +0200

    Updated nvim plugins, added keys for autosave

--- .config/nvim/lua/plugins/autosave.lua
+++ .config/nvim/lua/plugins/autosave.lua
@@ -3,13 +3,21 @@ return {
         'okuuva/auto-save.nvim',
         -- dev = true,
         event = 'VeryLazy',
+        keys = {
+            { '[oa', function() require('auto-save').on() end },
+            { ']oa', function() require('auto-save').off() end },
+            { 'yoa', function() require('auto-save').toggle() end },
+        },
         opts = {
+            execution_message = {
+                dim = 0.2,
+            },
             debounce_delay = 1000,
             condition = function(buf)
                 local fn = vim.fn
                 local utils = require('auto-save.utils.data')
 
-                -- don't save for special-buffers 
+                -- don't save for special-buffers
                 if fn.getbufvar(buf, '&buftype') ~= '' then
                     return false
                 end
--- .config/nvim/lua/toni/autocmds.lua
+++ .config/nvim/lua/toni/autocmds.lua
@@ -1,10 +1,12 @@
-local yank_group = vim.api.nvim_create_augroup('highlight_yank', {})
-vim.api.nvim_create_autocmd('TextYankPost', {
+local api = vim.api
+
+local yank_group = api.nvim_create_augroup('highlight_yank', {})
+api.nvim_create_autocmd('TextYankPost', {
     command = 'silent! lua vim.highlight.on_yank({timeout=700})',
     group = yank_group,
 })
 
-vim.api.nvim_create_autocmd(
+api.nvim_create_autocmd(
     'BufReadPost',
     { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
 )
