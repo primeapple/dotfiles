74abfe0a2e44c8f15be5b5d15116d1b2e8f269a4
primeapple <toni.mueller.web@mailbox.org>
2024-03-05 22:18:17 +0100

    Minor changes

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -17,8 +17,6 @@ return {
                     'css',
                     'dockerfile',
                     'fish',
-                    'gitcommit',
-                    'gitignore',
                     'html',
                     'java',
                     'javascript',
