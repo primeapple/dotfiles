1b251a289fe461a224813dae30d8289602bdcf1c
primeapple <toni.mueller.web@mailbox.org>
2024-03-19 14:29:18 +0100

    Remove friendly snippets

--- .config/nvim/lua/plugins/snippets.lua
+++ .config/nvim/lua/plugins/snippets.lua
@@ -2,11 +2,5 @@ return {
     {
         'L3MON4D3/LuaSnip',
         event = 'VeryLazy',
-        dependencies = {
-            'rafamadriz/friendly-snippets',
-            config = function()
-                require('luasnip.loaders.from_vscode').lazy_load()
-            end,
-        },
     },
 }
