f91574c931fa309fd0307d3f3b2a1b0af24ebdb2
primeapple <toni.mueller.web@mailbox.org>
2024-03-19 15:45:51 +0100

    Attempt to get clamshell running

---
+++ .config/hypr/scripts/clamshell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+
+if [[ "$(hyprctl monitors)" =~ "\sDP-[0-9]+" ]]; then
+  if [[ $1 == "open" ]]; then
+    hyprctl keyword monitor "eDP-1,3840x2400@60,320x1440,2"
+  else
+    hyprctl keyword monitor "eDP-1,disable"
+  fi
+fi
--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 FILE_NAME="screenshot-$(date +%F-%T).png"
 FILE_PATH="${HOME}/pictures/screenshots/${FILE_NAME}"
