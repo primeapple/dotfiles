98042fc4e9e33a44290bb0406fc1f49d5231c473
primeapple <toni.mueller.web@mailbox.org>
2024-08-09 16:06:27 +0200

    Fix Clamshell mode for hyprland

--- .config/hypr/scripts/clamshell.sh
+++ .config/hypr/scripts/clamshell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ "$(hyprctl monitors)" =~ "\sDP-[0-9]+" ]]; then
+if [[ "$(hyprctl monitors)" =~ [[:space:]]DP-[0-9]+ ]]; then
   if [[ $1 == "open" ]]; then
     hyprctl keyword monitor "eDP-1,3840x2400@60,320x1440,2"
   else
