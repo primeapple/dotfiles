a74c5d7a168b582cdd87ef14e6f09cb807f389ab
primeapple <toni.mueller.web@mailbox.org>
2024-08-23 06:47:52 +0200

    Added clamshell toggle command

--- .config/hypr/scripts/clamshell.sh
+++ .config/hypr/scripts/clamshell.sh
@@ -1,10 +1,51 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ "$(hyprctl monitors)" =~ [[:space:]]DP-[0-9]+ ]]; then
-  if [[ $1 == "open" ]]; then
+open() {
     hyprctl keyword monitor "eDP-1,3840x2400@60,320x1440,2"
-  else
+}
+
+close() {
     hyprctl keyword monitor "eDP-1,disable"
-  fi
-fi
+}
+
+toggle() {
+    if [[ "$(hyprctl monitors)" =~ eDP-[0-9]+ ]]; then
+        close
+    else
+        open
+    fi
+}
+
+
+main() {
+    if ! [[ "$(hyprctl monitors)" =~ [[:space:]]DP-[0-9]+ ]]; then
+        echo "No second monitor found, aborting"
+        exit 0
+    fi
+
+    local usage="Usage: $0 {open|close|toggle}"
+    
+    if [ $# -ge 1 ]; then
+        case "$1" in
+            open)
+                open
+                ;;
+            close)
+                close
+                ;;
+            toggle)
+                toggle
+                ;;
+            *)
+                echo "$usage"
+                exit 1
+                ;;
+        esac
+    else
+        echo "$usage"
+        exit 1
+    fi
+}
+
+main "$@"
