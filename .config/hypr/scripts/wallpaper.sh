c19a124dd6a7951420b7a23be503d9fbddbec636
primeapple <toni.mueller.web@mailbox.org>
2024-10-07 21:21:34 +0200

    Fix hyprland config

---
+++ .config/hypr/scripts/wallpaper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+directory=~/pictures/wallpaper/
+monitor=$(hyprctl monitors | grep Monitor | awk '{print $2}')
+
+if [ -d "$directory" ]; then
+    random_background=$(find $directory -type f -name "*.png" | shuf -n 1)
+
+    hyprctl hyprpaper unload all
+    hyprctl hyprpaper preload "$random_background"
+    hyprctl hyprpaper wallpaper "$monitor, $random_background"
+fi
