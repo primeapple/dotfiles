72d0f01ef3170cf39553b03dc58d12c077d47f08
primeapple <toni.mueller.web@mailbox.org>
2024-10-08 11:32:40 +0200

    Make wallpaper script work for multiple monitors

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 directory=~/pictures/wallpaper/
-monitor=$(hyprctl monitors | grep Monitor | awk '{print $2}')
+monitors=$(hyprctl monitors | grep Monitor | awk '{print $2}')
 
 if [ -d "$directory" ]; then
     random_background=$(find $directory -type f -name "*.png" | shuf -n 1)
 
     hyprctl hyprpaper unload all
     hyprctl hyprpaper preload "$random_background"
-    hyprctl hyprpaper wallpaper "$monitor, $random_background"
+    for monitor in $monitors ; do
+        hyprctl hyprpaper wallpaper "$monitor, $random_background"
+    done
 fi
