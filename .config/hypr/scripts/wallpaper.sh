0e65316d9e62c6d76cd64c6e1f161b82b8bdc3b0
primeapple <toni.mueller.web@mailbox.org>
2024-10-17 09:36:16 +0200

    Fixed wallpaper script to wait for `hyprpaper` to start

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 directory=~/pictures/wallpaper/
-monitors=$(hyprctl monitors | grep Monitor | awk '{print $2}')
+TIMEOUT_AFTER_SECONDS=50
 
-if [ -d "$directory" ]; then
-    random_background=$(find $directory -type f -name "*.png" | shuf -n 1)
+for ((i=1; i<=TIMEOUT_AFTER_SECONDS; i++)); do
+    if hyprctl hyprpaper > /dev/null 2>&1; then
+        monitors=$(hyprctl monitors | grep Monitor | awk '{print $2}')
+        if [ -d "$directory" ]; then
+            random_background=$(find $directory -type f -name "*.png" | shuf -n 1)
 
-    hyprctl hyprpaper unload all
-    hyprctl hyprpaper preload "$random_background"
-    for monitor in $monitors ; do
-        hyprctl hyprpaper wallpaper "$monitor, $random_background"
-    done
-fi
+            hyprctl hyprpaper unload all
+            hyprctl hyprpaper preload "$random_background"
+            for monitor in $monitors ; do
+                hyprctl hyprpaper wallpaper "$monitor, $random_background"
+            done
+        fi
+        break
+    else
+        sleep 1
+    fi
+done
