38bb76f1aef12f10cb8aab31d39d047976e4067b
primeapple <toni.mueller.web@mailbox.org>
2025-01-13 22:15:53 +0100

    Try out Neocodeium

---
+++ .config/nvim/lua/plugins/neocodeium.lua
@@ -0,0 +1,24 @@
+return {
+    {
+        'monkoose/neocodeium',
+        event = 'VeryLazy',
+        config = function()
+            local neocodeium = require('neocodeium')
+            neocodeium.setup({
+                manual = true,
+            })
+
+            -- close nvim cmp when ai completions are displayed
+            vim.api.nvim_create_autocmd('User', {
+                pattern = 'NeoCodeiumCompletionDisplayed',
+                callback = function()
+                    require('cmp').abort()
+                end,
+            })
+            vim.keymap.set('i', '<A-Space>',  neocodeium.cycle_or_complete)
+            vim.keymap.set('i', '<A-l>', neocodeium.accept)
+            vim.keymap.set('i', '<A-f>', neocodeium.accept_word)
+            vim.keymap.set('i', '<C-f>', neocodeium.accept_line)
+        end,
+    },
+}
