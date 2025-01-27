6cd16cb2a2990794d32b9c65c11ec4f1ce4ae92c
primeapple <toni.mueller.web@mailbox.org>
2025-01-27 07:13:25 +0100

    Added `gx.nvim` plugin

--- .config/nvim/lua/plugins/other.lua
+++ .config/nvim/lua/plugins/other.lua
@@ -255,4 +255,13 @@ return {
         event = { 'CursorMoved', 'WinScrolled' },
         opts = {},
     },
+    {
+        'chrishrb/gx.nvim',
+        keys = { { 'gx', '<cmd>Browse<cr>', mode = { 'n', 'x' } } },
+        cmd = { 'Browse' },
+        init = function()
+            vim.g.netrw_nogx = 1 -- disable netrw gx
+        end,
+        config = true, -- default settings
+    },
 }
