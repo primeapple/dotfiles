cff723b4f76f1563225ce29015895e15eaf01955
primeapple <toni.mueller.web@mailbox.org>
2025-04-28 11:43:52 +0200

    Added bookmarker init script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,15 @@
 source ~/.profile
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-
     # set keybindings to be vi + emacs
     set -g fish_key_bindings fish_hybrid_key_bindings
 
     # default is 50 iirc
     set -g fish_escape_delay_ms 10
+
+    if command -v bookmarker >/dev/null
+        bookmarker --init fish | source
+    end
 end
 
 set --export BUN_INSTALL "$HOME/.bun"
@@ -18,4 +20,3 @@ for path_to_add in ~/.local/share/JetBrains/Toolbox/scripts ~/bin $BUN_INSTALL/b
         fish_add_path -g $path_to_add
     end
 end
-
