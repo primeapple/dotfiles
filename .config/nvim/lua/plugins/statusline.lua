258d923d781842ad2030a95186da4df62cd14c85
primeapple <toni.mueller.web@mailbox.org>
2025-07-20 16:32:59 +0200

    Use heirline.nvim instead of feline.nvim

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -2,7 +2,6 @@ return {
     {
         'rebelot/heirline.nvim',
         event = 'UIEnter',
-        enabled = false,
         dependencies = {
             'nvim-lua/plenary.nvim',
             'nvim-tree/nvim-web-devicons',
@@ -12,7 +11,27 @@ return {
         config = function()
             local conditions = require('heirline.conditions')
             local utils = require('heirline.utils')
-            local colors = require('kanagawa.colors').setup()
+            local setup_colors = function()
+                return {
+                    fg = utils.get_highlight('Normal').fg,
+                    bg = utils.get_highlight('Normal').bg,
+                    red = utils.get_highlight('DiagnosticError').fg,
+                    dark_red = utils.get_highlight('DiffDelete').bg,
+                    green = utils.get_highlight('String').fg,
+                    blue = utils.get_highlight('Function').fg,
+                    gray = utils.get_highlight('NonText').fg,
+                    orange = utils.get_highlight('Constant').fg,
+                    purple = utils.get_highlight('Statement').fg,
+                    cyan = utils.get_highlight('Special').fg,
+                    diag_warn = utils.get_highlight('DiagnosticWarn').fg,
+                    diag_error = utils.get_highlight('DiagnosticError').fg,
+                    diag_hint = utils.get_highlight('DiagnosticHint').fg,
+                    diag_info = utils.get_highlight('DiagnosticInfo').fg,
+                    git_del = utils.get_highlight('diffDeleted').fg,
+                    git_add = utils.get_highlight('diffAdded').fg,
+                    git_change = utils.get_highlight('diffChanged').fg,
+                }
+            end
 
             local FileNameBlock = {
                 init = function(self)
@@ -37,20 +56,13 @@ return {
 
             local FileName = {
                 provider = function(self)
-                    -- first, trim the pattern relative to the current directory. For other
-                    -- options, see :h filename-modifers
-                    local filename = vim.fn.fnamemodify(self.filename, ':.')
+                    local filename = vim.fn.fnamemodify(self.filename, ':t')
                     if filename == '' then
                         return '[No Name]'
                     end
-                    -- now, if the filename would occupy more than 1/4th of the available
-                    -- space, we trim the file path to its initials
-                    if not conditions.width_percent_below(#filename, 0.25) then
-                        filename = vim.fn.pathshorten(filename)
-                    end
                     return filename
                 end,
-                hl = { fg = utils.get_highlight('Directory').fg },
+                hl = { bold = true },
             }
 
             local FileFlags = {
@@ -58,71 +70,56 @@ return {
                     condition = function()
                         return vim.bo.modified
                     end,
-                    provider = '[+]',
-                    -- TODO white
-                    hl = { fg = 'green' },
+                    provider = '● ',
+                    hl = { bold = true },
                 },
                 {
                     condition = function()
                         return not vim.bo.modifiable or vim.bo.readonly
                     end,
-                    provider = '',
-                    -- TODO white
+                    provider = ' ',
                     hl = { fg = 'orange' },
                 },
             }
 
-            -- Now, let's say that we want the filename color to change if the buffer is
-            -- modified. Of course, we could do that directly using the FileName.hl field,
-            -- but we'll see how easy it is to alter existing components using a "modifier"
-            -- component
-
-            local FileNameModifer = {
-                hl = function()
-                    if vim.bo.modified then
-                        -- use `force` because we need to override the child's hl foreground
-                        return { fg = 'cyan', bold = true, force = true }
-                    end
-                end,
-            }
-
-            -- let's add the children to our FileNameBlock component
             FileNameBlock = utils.insert(
                 FileNameBlock,
-                FileIcon,
-                utils.insert(FileNameModifer, FileName), -- a new table where FileName is a child of FileNameModifier
                 FileFlags,
-                { provider = '%<' } -- this means that the statusline is cut here when there's not enough space
+                FileIcon,
+                FileName,
+                { provider = '%<' } -- This means that the statusline is cut here when there's not enough space
             )
 
             local FileType = {
                 provider = function()
                     return string.upper(vim.bo.filetype)
                 end,
-                hl = { fg = utils.get_highlight('Type').fg, bold = true },
+                hl = { bold = true },
             }
 
             local Ruler = {
-                -- %l = current line number
-                -- %L = number of lines in the buffer
-                -- %c = column number
-                -- %P = percentage through file of displayed window
-                -- provider = "%7(%l/%3L%):%2c %P",
-                provider = '%7(%l):%2c %P',
+                provider = function()
+                    local lines = vim.api.nvim_buf_line_count(0)
+                    return string.format('%%%dl:%%2c %%P', #tostring(lines))
+                    -- return '%5l:%2c %P',
+                end,
+                hl = { bold = true },
             }
 
             local ScrollBar = {
                 static = {
-                    -- TODO use better icons, start from top to bottom
                     sbar = { '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█' },
                 },
                 provider = function(self)
                     local curr_line = vim.api.nvim_win_get_cursor(0)[1]
                     local lines = vim.api.nvim_buf_line_count(0)
-                    local i = math.floor((curr_line - 1) / lines * #self.sbar) + 1
-                    return string.rep(self.sbar[i], 2)
+                    return string.rep(self.sbar[8 - math.floor(curr_line / lines * 7)], 2)
                 end,
-                hl = { fg = 'blue', bg = 'bright_bg' },
+                hl = {
+                    bg = 'fg',
+                    fg = 'bg',
+                    bold = true,
+                },
             }
 
             local LSPActive = {
@@ -130,23 +127,23 @@ return {
                 update = { 'LspAttach', 'LspDetach' },
                 provider = function()
                     local names = {}
-                    for i, server in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
+                    for _, server in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
                         table.insert(names, server.name)
                     end
-                    return ' [' .. table.concat(names, ' ') .. ']'
+                    return ' ' .. table.concat(names, ' ')
                 end,
-                hl = { fg = 'green', bold = true },
+                hl = { fg = 'purple', bold = true },
             }
 
             local Diagnostics = {
-
                 condition = conditions.has_diagnostics,
 
+                -- Fetching custom diagnostic icons
                 static = {
-                    error_icon = vim.fn.sign_getdefined('DiagnosticSignError')[1].text,
-                    warn_icon = vim.fn.sign_getdefined('DiagnosticSignWarn')[1].text,
-                    info_icon = vim.fn.sign_getdefined('DiagnosticSignInfo')[1].text,
-                    hint_icon = vim.fn.sign_getdefined('DiagnosticSignHint')[1].text,
+                    error_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.ERROR],
+                    warn_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.WARN],
+                    info_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.INFO],
+                    hint_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.HINT],
                 },
 
                 init = function(self)
@@ -161,25 +158,25 @@ return {
                 {
                     provider = function(self)
                         -- 0 is just another output, we can decide to print it or not!
-                        return self.errors > 0 and (self.error_icon .. self.errors .. ' ')
+                        return self.errors > 0 and (self.error_icon .. ' ' .. self.errors .. ' ')
                     end,
                     hl = { fg = 'diag_error' },
                 },
                 {
                     provider = function(self)
-                        return self.warnings > 0 and (self.warn_icon .. self.warnings .. ' ')
+                        return self.warnings > 0 and (self.warn_icon .. ' ' .. self.warnings .. ' ')
                     end,
                     hl = { fg = 'diag_warn' },
                 },
                 {
                     provider = function(self)
-                        return self.info > 0 and (self.info_icon .. self.info .. ' ')
+                        return self.info > 0 and (self.info_icon .. ' ' .. self.info .. ' ')
                     end,
                     hl = { fg = 'diag_info' },
                 },
                 {
                     provider = function(self)
-                        return self.hints > 0 and (self.hint_icon .. self.hints)
+                        return self.hints > 0 and (self.hint_icon .. ' ' .. self.hints)
                     end,
                     hl = { fg = 'diag_hint' },
                 },
@@ -192,7 +189,7 @@ return {
                     self.status_dict = vim.b.gitsigns_status_dict
                 end,
 
-                hl = { fg = 'orange' },
+                hl = { fg = 'purple' },
 
                 { -- git branch name
                     provider = function(self)
@@ -200,43 +197,56 @@ return {
                         if #branch > 14 then
                             branch = string.sub(branch, 1, 14) .. '…'
                         end
-                        return ' ' .. branch
+                        return ' ' .. branch .. ' '
                     end,
                     hl = { bold = true },
                 },
-                -- TODO use icons
                 {
                     provider = function(self)
                         local count = self.status_dict.added or 0
-                        return count > 0 and ('+' .. count)
+                        return count > 0 and ('  ' .. count)
                     end,
                     hl = { fg = 'git_add' },
                 },
                 {
                     provider = function(self)
                         local count = self.status_dict.removed or 0
-                        return count > 0 and ('-' .. count)
+                        return count > 0 and ('  ' .. count)
                     end,
                     hl = { fg = 'git_del' },
                 },
                 {
                     provider = function(self)
                         local count = self.status_dict.changed or 0
-                        return count > 0 and ('~' .. count)
+                        return count > 0 and ('  ' .. count)
                     end,
                     hl = { fg = 'git_change' },
                 },
             }
             local Align = { provider = '%=' }
-            local Space = { provider = ' ' }
+            local Space = { provider = '  ' }
 
-            local DefaultStatuslineLeft = { FileNameBlock, Git, Align }
-            local DefaultStatusLineRight = { LSPActive, Diagnostics, FileType, Ruler, ScrollBar }
-            local DefaultStatusLine = { DefaultStatuslineLeft, DefaultStatusLineRight }
+            local DefaultStatuslineLeft = { FileNameBlock, Space, Git, Align }
+            local DefaultStatusLineRight = {
+                LSPActive,
+                Space,
+                Diagnostics,
+                Space,
+                FileType,
+                Space,
+                Ruler,
+                Space,
+                ScrollBar,
+            }
+            local DefaultStatusLine = {
+                DefaultStatuslineLeft,
+                DefaultStatusLineRight,
+                hl = { bg = 'bg' },
+            }
 
             require('heirline').setup({
                 opts = {
-                    colors = colors,
+                    colors = setup_colors(),
                 },
                 statusline = { DefaultStatusLine },
             })
@@ -245,7 +255,7 @@ return {
     {
         'freddiehaddad/feline.nvim',
         event = 'UIEnter',
-        -- enabled = false,
+        enabled = false,
         dependencies = {
             'nvim-lua/plenary.nvim',
             'nvim-tree/nvim-web-devicons',
@@ -444,7 +454,8 @@ return {
         -- TODO: works for now, but may be better with a check if the current terminal is kitty
         cond = require('toni.utils').is_workstation,
         event = 'UIEnter',
-        dependencies = { 'freddiehaddad/feline.nvim' },
+        enabled = false,
+        dependencies = { 'rebelot/heirline.nvim' },
         init = function()
             vim.cmd('let g:tpipeline_size = &co')
             vim.g.tpipeline_clearstl = 1
