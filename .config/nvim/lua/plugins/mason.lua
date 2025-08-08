5ed037273be21f6640f55cdef185cb0d46fba5a1
primeapple <toni.mueller.web@mailbox.org>
2025-08-08 21:59:46 +0200

    feat(nvim): Remove mason-lspconfig and feline.nvim

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -2,9 +2,6 @@ return {
     'mason-org/mason.nvim',
     cmd = 'Mason',
     cond = require('toni.utils').is_workstation,
-    dependencies = {
-        'mason-org/mason-lspconfig.nvim',
-    },
     config = function()
         require('mason').setup({
             ui = {
@@ -13,12 +10,6 @@ return {
                     server_pending = '➜',
                     server_uninstalled = '✗',
                 },
-            },
-        })
-
-        require('mason-lspconfig').setup({
-            ensure_installed = {
-                'jdtls',
             },
         })
     end,
