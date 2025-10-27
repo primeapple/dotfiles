b1fed67f4a66897608f5daace9e518f735184a27
primeapple <toni.mueller.web@mailbox.org>
2025-10-27 22:20:36 +0100

    feat(fish): Start week at monday with `rn`

--- .config/fish/functions/rn.fish
+++ .config/fish/functions/rn.fish
@@ -1,5 +1,5 @@
 function rn --description 'Prints today and current month'
     date "+%l:%M%p on %A, %B %e, %Y"
     echo
-    cal | grep -E "\b$(date '+%e')\b| "
+    cal --monday | grep -E "\b$(date '+%e')\b| "
 end
