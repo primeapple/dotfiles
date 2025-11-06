701029e957fe23b71251c4d71b751ad6ff001cc5
primeapple <toni.mueller.web@mailbox.org>
2025-11-06 22:34:22 +0100

    feat(opencode): Added .env protection

---
+++ .config/opencode/plugin/env-protection.js
@@ -0,0 +1,9 @@
+export const EnvProtection = async ({ project, client, $, directory, worktree }) => {
+  return {
+    "tool.execute.before": async (input, output) => {
+      if (input.tool === "read" && output.args.filePath.includes(".env")) {
+        throw new Error("Do not read .env files")
+      }
+    },
+  }
+}
