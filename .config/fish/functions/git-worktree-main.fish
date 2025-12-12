c34f7b64afd2bffc8271b39462329d225bd77062
primeapple <toni.mueller.web@mailbox.org>
2025-12-12 10:54:32 +0100

    fix(git): Adjusted worktree scripts

--- .config/fish/functions/git-worktree-main.fish
+++ .config/fish/functions/git-worktree-main.fish
@@ -1,3 +1,7 @@
-function git-worktree-main --description 'Change to the main worktree'
-    cd (git worktree list | grep '+main' | awk '{print $1}')
+function git-worktree-main --description 'Change to the main/master worktree'
+    if git rev-parse --verify main &>/dev/null
+        cd (git worktree list | grep '+main' | awk '{print $1}')
+    else
+        cd (git worktree list | grep '+master' | awk '{print $1}')
+    end
 end
