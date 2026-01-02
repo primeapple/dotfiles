b1dbc80781b678f2fac3c4c13ee7994459147185
primeapple <toni.mueller.web@mailbox.org>
2026-01-02 07:34:25 +0100

    chore(nvim): Update plugins

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
     {
-        'echasnovski/mini.nvim',
+        'nvim-mini/mini.nvim',
         version = '*',
         event = 'VeryLazy',
         dependencies = { 'rafamadriz/friendly-snippets' },
