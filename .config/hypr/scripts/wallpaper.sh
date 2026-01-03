37d7689e1823ab2bce6397549fc71d463ae2781e
primeapple <toni.mueller.web@mailbox.org>
2026-01-03 12:57:36 +0100

    fix(hyprland): Adjusted wallpaper script

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -4,13 +4,11 @@ directory=~/pictures/wallpaper/
 TIMEOUT_AFTER_SECONDS=50
 
 for ((i=1; i<=TIMEOUT_AFTER_SECONDS; i++)); do
-    if hyprctl hyprpaper > /dev/null 2>&1; then
+    if pgrep -x "hyprpaper" > /dev/null; then
         monitors=$(hyprctl monitors | grep Monitor | awk '{print $2}')
         if [ -d "$directory" ]; then
             random_background=$(find $directory -type f -name "*.png" | shuf -n 1)
 
-            hyprctl hyprpaper unload all
-            hyprctl hyprpaper preload "$random_background"
             for monitor in $monitors ; do
                 hyprctl hyprpaper wallpaper "$monitor, $random_background"
             done
