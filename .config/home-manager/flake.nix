aa405093a7faf537f4ea87e3d4f719242cce1369
primeapple <toni.mueller.web@mailbox.org>
2026-02-02 17:05:45 +0100

    feat(nix): Enabled flake for home-manager

---
+++ .config/home-manager/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Home Manager configuration of toni";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    homeConfigurations."toni" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      modules = [ ./home.nix ];
+    };
+  };
+}
