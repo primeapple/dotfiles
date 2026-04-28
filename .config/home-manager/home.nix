da2467ae78e9f25c9c85274bff4f32138a3219ef
primeapple <toni.mueller.web@mailbox.org>
2026-04-28 20:03:48 +0200

    fix: Make llama-cpp and zf Linux-only in home.nix
    - zf fails to build on aarch64-darwin (zig build issue)
    - llama-cpp is Linux-only anyway

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -22,17 +22,12 @@ in
     fd
     htop
     jq
-    llama-cpp
     neovim
     ripgrep
     trash-cli
+  ] ++ lib.optionals (!isDarwin) [
+    llama-cpp
     zf
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
