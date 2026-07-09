6892dbc5674eb91ff2759da5772ae847de57e816
primeapple <toni.mueller.web@mailbox.org>
2026-07-09 15:42:50 +0200

    fix(fish): Set correct `AWS_PROFILE` for aws login

--- .config/fish/functions/aws:login.fish
+++ .config/fish/functions/aws:login.fish
@@ -2,6 +2,6 @@ function aws:login
     set -l profile (string replace --all --regex '\[|\]' '' (string replace -r 'profile ' '' (grep profile ~/.aws/config)) | zf)
     echo "Login into AWS using profile $profile"
     aws sso login --profile $profile
-    set -gx AWS_PROFILE $profile
+    set -Ux AWS_PROFILE $profile
     aws sts get-caller-identity
 end
