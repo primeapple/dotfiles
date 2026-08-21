94953f18049f53fd9237cac769f2de032ef75f2d
primeapple <toni.mueller.web@mailbox.org>
2026-08-21 10:12:14 +0200

    feat(nvim): Fix java lsp

--- .config/nvim/lua/plugins/languages/java.lua
+++ .config/nvim/lua/plugins/languages/java.lua
@@ -4,11 +4,34 @@ return {
     ft = 'java',
     config = function()
         vim.lsp.config("jdtls", {
+            root_markers = { "gradlew", "settings.gradle.kts", ".git" },
+            -- TODO use $SDKMAN_DIR
+            cmd_env = {
+                JAVA_HOME = "/opt/homebrew/opt/sdkman-cli/libexec/candidates/java/21.0.12-amzn",
+            },
             settings = {
                 java = {
-                    -- Custom eclipse.jdt.ls options go here
-                },
-            },
+                    import = {
+                        gradle = {
+                            wrapper = {
+                                enabled = true,
+                            },
+                        },
+                    },
+                    configuration = {
+                        runtimes = {
+                            {
+                                name = "JavaSE-21",
+                                path = "/opt/homebrew/opt/sdkman-cli/libexec/candidates/java/21.0.12-amzn",
+                            },
+                            {
+                                name = "JavaSE-26",
+                                path = "/opt/homebrew/opt/sdkman-cli/libexec/candidates/java/26-amzn",
+                            }
+                        },
+                    }
+                }
+            }
         })
         vim.lsp.enable("jdtls")
     end,
